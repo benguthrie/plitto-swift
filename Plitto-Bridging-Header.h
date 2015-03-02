ca5eade0efb166ba5bb0d0fc3889b5d6da3d5316
Ben Guthrie <guthrie.ben@gmail.com>
2015-03-02 16:04:17 -0600

    Auto-layout complete

---
+++ Plitto-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Plitto-Bridging-Header.h
+//  Plitto E
+//
+//  Created by Ben Guthrie on 2/27/15.
+//  Copyright (c) 2015 Ben Guthrie. All rights reserved.
+//
+
+#ifndef Plitto_E_Plitto_Bridging_Header_h
+#define Plitto_E_Plitto_Bridging_Header_h
+
+
+#endif
