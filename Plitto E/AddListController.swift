23522bf398a6f830bad34796147cd44c0c1567e6
Ben Guthrie <guthrie.ben@gmail.com>
2015-03-06 16:11:00 -0600

    Add to list started.

---
+++ Plitto E/AddListController.swift
@@ -0,0 +1,30 @@
+//
+//  AddListController.swift
+//  Plitto E
+//
+//  Created by Ben Guthrie on 3/6/15.
+//  Copyright (c) 2015 Ben Guthrie. All rights reserved.
+//
+
+import UIKit
+
+
+class AddListController: UIViewController {
+
+  @IBOutlet var addListText: UITextField!
+
+  @IBOutlet var AddListContext: UILabel!
+
+  @IBAction func addButtonPressed(sender: UIButton) {
+    println("Add button Pressed")
+    println(addListText.text)
+  }
+
+  @IBAction func textChanged(sender: AnyObject) {
+    println("Text Changed")
+    println(addListText.text)
+    // TODO - This event is not being triggered.
+
+  }
+
+}
