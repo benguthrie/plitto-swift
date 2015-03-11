ba7c2aaf9e4283626449a47e05b8153be33794a1
Ben Guthrie <guthrie.ben@gmail.com>
2015-03-10 20:02:19 -0500

    Add to list - Ready for Search Results.

--- Plitto E/AddListController.swift
+++ Plitto E/AddListController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 
-class AddListController: UIViewController {
+class AddListController: UIViewController, UITextFieldDelegate {
 
   @IBOutlet var addListText: UITextField!
 
@@ -17,20 +17,66 @@ class AddListController: UIViewController {
 
   @IBAction func addButtonPressed(sender: UIButton) {
     println("Add button Pressed")
-    println(addListText.text)
+    // println(addListText.text)
+    addToList()
   }
 
   @IBAction func AddListSubmit(sender: AnyObject) {
-    println("Editing ended")
+    println("Editing ended. Probably by return. ")
+    addToList()
   }
-  
+
+
   @IBAction func textChanged(sender: AnyObject) {
     println("Text Changed")
-    println(addListText.text)
+    // println(addListText.text)
+    suggestList()
     // TODO - This event is not being triggered.
     // Based on this event, the search should be triggered
-    var AddListContext = "Updated"
+    // AddListContext.text = "Updated"
 
+  }
+
+  override func viewDidLoad(){
+
+    super.viewDidLoad()
+
+
+    addListText.delegate = self
+
+    AddListContext.text = "Search Above. Results Below."
+
+
+  }
+
+  func textFieldShouldReturn(theField: UITextField!) -> Bool {
+    theField.resignFirstResponder()
+    // println("first responder. return text")
+    //   addToList()
+    return true
+  }
+
+  func suggestList() -> String {
+
+    var stringLength = countElements(addListText.text)
+
+
+    println("suggest a list \(addListText.text) \(stringLength)")
+
+    if stringLength == 0 {
+      AddListContext.text = "Search Above"
+      println("String was 0")
+    } else {
+      AddListContext.text = "Results for: \(addListText.text)"
+      println("String wwas > 0")
+    }
+
+    return "string"
+  }
+
+  func addToList() -> String {
+    println("Add To List called \(addListText.text)")
+    return "string"
   }
 
 }
